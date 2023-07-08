cf54168c7b407879a7e02e592c6218e4ebdf5ae5
Sanderson Machado <sandersonoficial10@hotmail.com>
2023-07-08 17:05:25 -0300

    rapaz

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    question: "Quanto é 1 + 2?",
+    answers: [
+      {option: "3", correct: true},
+      {option: "6", correct: false},
+      {option: "7", correct: false},
+    ],
+  },
+  {
+    question: "Quanto é 1 + 2?",
+    answers: [
+      {option: "3", correct: true},
+      {option: "6", correct: false},
+      {option: "7", correct: false},
+    ],
+  },
+  {
+    question: "Quanto é 1 + 2?",
+    answers: [
+      {option: "3", correct: true},
+      {option: "6", correct: false},
+      {option: "7", correct: false},
+    ],
+  },
+  {
+    question: "Quanto é 1 + 2?",
+    answers: [
+      {option: "3", correct: true},
+      {option: "6", correct: false},
+      {option: "7", correct: false},
+    ],
+  },
+]
